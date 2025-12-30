173d5c5a689dc0f6a3087d476a86c0d21ff22bb7
PyScriptPlus <mohamadexbier@gmail.com>
2025-12-30 19:55:03 +0330

    Writing code about Circle_Physics with C++ and Library RayLib

---
+++ RayLib/Football.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include "raylib.h"
+#include <raymath.h>
+
+int main()
+{
+    const int screenWidth = 1366;
+    const int screenHeight = 768;
+    InitWindow(screenWidth, screenHeight, "Football");
+    SetTargetFPS(100);
+    //SetTargetFPS(10);
+
+    float taghsim = screenWidth / 2.0f;
+    float taghsim2 = screenHeight / 2.0f;
+    Vector2 circlePos = { taghsim, taghsim2 };
+    float radius = 50.0f;
+    float speed = 5.0f;
+
+    int num = 0;
+    while (!WindowShouldClose())
+    {
+
+        if (IsKeyDown(KEY_RIGHT))
+            circlePos.x += speed;
+        if (IsKeyDown(KEY_LEFT))
+            circlePos.x -= speed;
+        if (IsKeyDown(KEY_UP))
+            circlePos.y -= speed;
+        if (IsKeyDown(KEY_DOWN))
+            circlePos.y += speed;
+
+
+        BeginDrawing();
+        ClearBackground(GREEN);
+
+
+        //DrawCircle3D(circlePos,radius, circlePos,radius,RED);
+        //DrawFPS(5, 5);
+        //DrawText("Coded By MamaD", 10, 50, 100, RED);
+        DrawText(TextFormat("FPS: %i", GetFPS()), 10, 10, 20, BLUE);
+        DrawCircleV(circlePos, radius, PURPLE);
+        //DrawCircle(GetMouseX(), GetMouseY(), radius, YELLOW);
+
+
+
+
+        /*
+            float x;     // Rectangle top-left corner position x
+            float y;     // Rectangle top-left corner position y
+            float width; // Rectangle width
+            float height;// Rectangle height
+        */
+        Rectangle rec = { 0.0, 0.0,screenWidth,screenHeight };
+        /*    if (CheckCollisionCircleRec(circlePos,radius,rec))
+            {
+
+
+
+            }
+            else
+            {
+            }*/
+        if (circlePos.x - radius <= rec.x)
+        {
+            speed = 0;
+            if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_UP) || IsKeyDown(KEY_DOWN))
+            {
+                if (!IsKeyDown(KEY_LEFT))
+                {
+                    speed = 5.0f;
+                }
+
+            }
+        }
+        if (circlePos.y - radius <= rec.y)
+        {
+            std::cout << "Barkhord bala!!\n";
+        }
+        if (circlePos.y + radius >= rec.y + rec.height) {
+            std::cout << "payin\n";
+        }
+        if (circlePos.x + radius >= rec.x + rec.width)
+        {
+            std::cout << "rast\n";
+        }
+
+
+        //circle
+        //cirle
+        //DrawCircle(circlePos.x, circlePos.y, radius, PURPLE);
+        //Vector3 center3D = { 0.0f, 0.0f, 0.0f };
+        //Vector2 circlePos22 = { taghsim, taghsim2 };
+        /*int num = 0;
+        if (CheckCollisionCircles(circlePos, 0.0f, circlePos, 0.0f))
+        {
+            std::cout << "Barkhord kard: " << num << '\n';
+            num++;
+        }*/
+        //std::cout << "x: " << GetMouseX() << " Y: " << GetMouseY() << '\n';
+
+
+        //DrawCircleV(circlePos, radius, RED);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
